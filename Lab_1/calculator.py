8a9ab0ee47c4d349a1321e79e29bbead888f4051
mc <mcsx97@gmail.com>
2020-09-18 10:19:46 +0200

    First commit

---
+++ Lab_1/calculator.py
@@ -0,0 +1,27 @@
+#calculator.py
+
+def sum(m, n):
+    if n < 0:
+        for i in range(-n):
+            m -= 1
+    else:
+        for i in range(n):
+            m += 1
+    return m
+
+def divide(m, n):
+    k = 0
+    isNeg = (m > 0 and n < 0) or (m < 0 and n > 0)
+    n = abs(n)
+    m = abs(m)
+
+    while m > n:
+        m -= n
+        k += 1
+
+    if isNeg:
+        return -k
+    return k
+
+if __name__ == "__main__":
+    print(divide(28, -7))
